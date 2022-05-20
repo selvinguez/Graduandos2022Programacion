9f13ca8bf2963302bb3e1475537f6b7456f27da7
selvinguez <42685319+selvinguez@users.noreply.github.com>
2022-05-20 10:58:21 -0600

    Ejemplo 1

---
+++ Ejemplo.java
@@ -0,0 +1,9 @@
+/* Primer Ejemplo Hola Java - aprenderaprogramar.com */
+
+public class Ejemplo {
+    public static void main(String[] arg) {
+        System.out.println("Hola Java");
+        int suma = 1 + 1;
+        System.out.println(suma);
+    }
+}
