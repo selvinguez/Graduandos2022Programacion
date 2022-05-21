f84f60a249596e612020cb35a44b2bd13fd40dd9
selvinguez <42685319+selvinguez@users.noreply.github.com>
2022-05-21 15:01:47 -0600

    Create Ejemplo2.java

---
+++ Ejemplo2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {  
+  public static void main(String args[]) { 
+    List<Integer> list=new ArrayList<Integer>();
+    list.add(4);  
+    list.add(6);  
+    list.add(7);  
+    list.add(8); 
+    for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+  } 
+}
