9e3d98f107297fab08461024dabf702661da31d3
selvinguez <42685319+selvinguez@users.noreply.github.com>
2022-05-21 15:09:28 -0600

    Create Ejercicio5.java

---
+++ Ejercicio5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {  
+  public static void main(String args[]) { 
+    List<Integer> list=new ArrayList<Integer>();
+    list.add(4);  
+    list.add(6);  
+    list.add(7);  
+    list.add(8); 
+ 
+    for(int x:list){
+      System.out.println(x);
+    }
+  } 
+}
